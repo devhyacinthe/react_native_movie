daf9588d9b2d6ba487ea1b31c98ac4d0c13820f0
devhyacinthe <yacinthedalmeida2002@gmail.com>
2025-03-13 21:38:30 +0000

    Final Commit

--- components/TrendingMovieCard.tsx
+++ components/TrendingMovieCard.tsx
@@ -1,6 +1,8 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import MaskedView from "@react-native-masked-view/masked-view";
 import React from "react";
 import { Link } from "expo-router";
+import { images } from "@/constants/images";
 
 const TrendingMovieCard = ({
   movie: { id, title, poster_path },
@@ -28,8 +30,19 @@ const TrendingMovieCard = ({
         />
 
         <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
-
+          <MaskedView
+            maskElement={
+              <Text className="font-bold text-white text-6xl">{index + 1}</Text>
+            }
+          >
+            <Image
+              source={images.rankingGradient}
+              className="size-14"
+              resizeMode="cover"
+            />
+          </MaskedView>
         </View>
+        <Text className="text-sm font-bold mt-2 text-gray-200" numberOfLines={2}>{title}</Text>
       </TouchableOpacity>
     </Link>
   );
--- services/api.ts
+++ services/api.ts
@@ -1,55 +1,74 @@
-
-const MOVIE_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTk5OGMxYjk2MmU0ZTAxYzllNmU4M2I3NDcyNDE0ZSIsIm5iZiI6MTY1OTU0MDk5MC42NjYsInN1YiI6IjYyZWE5NWZlODU2NmQyMDA1YTQ4MmUzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NKJpfL1ahCSreRsFKtETZahUHUpZQipROHun4iiDuDo"
+const MOVIE_API_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTk5OGMxYjk2MmU0ZTAxYzllNmU4M2I3NDcyNDE0ZSIsIm5iZiI6MTY1OTU0MDk5MC42NjYsInN1YiI6IjYyZWE5NWZlODU2NmQyMDA1YTQ4MmUzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NKJpfL1ahCSreRsFKtETZahUHUpZQipROHun4iiDuDo";
 
 export const TMDB_CONFIG = {
-    BASE_URL: 'https://api.themoviedb.org/3',
-    API_KEY: MOVIE_API_KEY,
-    headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${MOVIE_API_KEY}`
-    }
-}
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${MOVIE_API_KEY}`,
+  },
+};
 
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `/search/movie?query=${encodeURIComponent(query)}`
+    : "/discover/movie?sort_by=popularity.desc";
 
-export const fetchMovies = async ({ query }: {query: string}) => {
-    const endpoint =  query ? `/search/movie?query=${encodeURIComponent(query)}` :
-    '/discover/movie?sort_by=popularity.desc'
+  const response = await fetch(`${TMDB_CONFIG.BASE_URL}${endpoint}`, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
 
-    const response = await fetch(`${TMDB_CONFIG.BASE_URL}${endpoint}`, {
-        method: 'GET',
-        headers: TMDB_CONFIG.headers
-    })
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
 
+  const data = await response.json();
 
-    if(!response.ok) {
-        // @ts-ignore
-        throw new Error('Failed to fetch movies', response.statusText)
-    }
-
-
-    const data = await response.json()
-
-    return data.results;
-}
-
+  return data.results;
+};
 
 export const fetchPopularMovies = async () => {
-    const endpoint = 
-    '/movie/popular?language=en-US&page=2'
+  const endpoint = "/movie/popular?language=en-US&page=2";
+
+  const response = await fetch(`${TMDB_CONFIG.BASE_URL}${endpoint}`, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch popular movies", response.statusText);
+  }
+
+  const data = await response.json();
+
+  return data.results;
+};
+
+export const fetchMoviesDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const endpoint = `/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`;
 
     const response = await fetch(`${TMDB_CONFIG.BASE_URL}${endpoint}`, {
-        method: 'GET',
-        headers: TMDB_CONFIG.headers
-    })
+      method: "GET",
+      headers: TMDB_CONFIG.headers,
+    });
 
-
-    if(!response.ok) {
-        // @ts-ignore
-        throw new Error('Failed to fetch popular movies', response.statusText)
+    if (!response.ok) {
+      // @ts-ignore
+      throw new Error("Failed to fetch movie details", response.statusText);
     }
 
+    const data = await response.json();
 
-    const data = await response.json()
-
-    return data.results;
-}
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
